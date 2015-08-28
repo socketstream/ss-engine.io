e40205e668c95edddf06ff00d55bef91190a7f32
Paul Jensen <paulbjensen@gmail.com>
2015-08-28 10:16:23 +0200

    Added a few tweaks

--- client/wrapper.js
+++ client/wrapper.js
@@ -5,7 +5,23 @@ var reconnectSwitch = false;
 var reconnectionTimeout = 1000;
 var ss = require('socketstream');
 
-module.exports = function(serverStatus, message, config){
+// Private
+
+var getCookie = function(c_name) {
+  var c_end, c_start;
+  if (document.cookie.length > 0) {
+    c_start = document.cookie.indexOf(c_name + '=');
+    if (c_start !== -1) {
+      c_start = c_start + c_name.length + 1;
+      c_end = document.cookie.indexOf(';', c_start);
+      if (c_end === -1) { c_end = document.cookie.length; }
+      return unescape(document.cookie.substring(c_start, c_end));
+    }
+  }
+  return '';
+};
+
+module.exports = function(serverStatus, message, config) {
 
   if (JSON.stringify(config) === '{}') {
     config = { 
@@ -97,21 +113,4 @@ module.exports = function(serverStatus, message, config){
       return sock.send; // (receives msg object)
     }
   };
-};
-
-
-// Private
-
-var getCookie = function(c_name) {
-  var c_end, c_start;
-  if (document.cookie.length > 0) {
-    c_start = document.cookie.indexOf(c_name + '=');
-    if (c_start !== -1) {
-      c_start = c_start + c_name.length + 1;
-      c_end = document.cookie.indexOf(';', c_start);
-      if (c_end === -1) c_end = document.cookie.length;
-      return unescape(document.cookie.substring(c_start, c_end));
-    }
-  }
-  return '';
 };
